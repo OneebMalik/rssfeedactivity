20d4b9c94d0c88858e459b81f850b36ea2a78caf
Oneeb Malik <oneeb@med.unc.edu>
2019-06-11 08:13:56 -0400

    Add basic feed class with getters and setters

--- src/feeds/Feed.java
+++ src/feeds/Feed.java
@@ -1,6 +1,52 @@
 package dev.oneeb.rssfeedactivity;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Feed {
+
+	// Required RSS channel elements.
+	private String title;
+	private String link;
+	private String description;
+
+	private String lastBuildDate;
+
 	public Feed() {
+		this.title = "";
+		this.link = "";
+		this.description = "";
+		this.lastBuildDate = "";
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public String getLink() {
+		return this.link;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	@Override
+	public String toString() {
+		return "Feed: {\n\t title: " + this.title +
+			"\n\t link: " + this.link + "\n\t description: " 
+			+ this.description + "\n}";
 	}
 }
